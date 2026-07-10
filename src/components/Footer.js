745b911cbe168ba47094f9c6e0400d8f9f1d4899
Sumit Chauhan <r.cgamerz2944@gmail.com>
2026-07-10 22:27:30 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -51,7 +51,7 @@ const Footer = () => {
                 <FaPhoneAlt className="info-icon" /> Phone: +1 647 962 9995
               </p>
               <p>
-                <FaEnvelope className="info-icon" /> Email: admin@gnmtransport.com
+                <FaEnvelope className="info-icon" /> Email: admin@gnmtransport.net
               </p>
             </div>
           </div>
